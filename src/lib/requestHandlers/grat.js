66b216e2d7584f4fcf74d87c9d88be59abe0f2a0
Anis Safine Laget <anis@users.noreply.github.com>
2019-05-01 13:09:19 +0200

    Improve the grat handler

--- src/lib/requestHandlers/grat.js
+++ src/lib/requestHandlers/grat.js
@@ -10,6 +10,15 @@ module.exports = function (config, page, helpers) {
         return message.match(/^[0-9]{2}h[0-9]{2}$/g) !== null;
     }
 
+    function reverse(str) {
+        var reversed = '';
+        for (var i = str.length - 1; i >= 0; i -= 1) {
+            reversed += str[i];
+        }
+
+        return reversed;
+    }
+
     /**
      * Checks if the given "hour message" matches the current time
      * 
@@ -20,8 +29,24 @@ module.exports = function (config, page, helpers) {
     function isMessageMatchingRealHour(hour) {
         var expected = hour.split('h');
         var now = new Date();
+        return now.getHours() === parseInt(expected[0], 10) && now.getMinutes() === parseInt(expected[1], 10);
+    }
 
-        return now.getHours() === parseInt(expected[0], 10) && now.getMinutes() === parseInt(expected[1]);
+    /**
+     * Checks if the given hour deserves a grat
+     *
+     * @param {string} hour
+     *
+     * @returns {boolean}
+     */
+    function isHourSpecial(hour) {
+        var expected = hour.split('h');
+        return expected[0] === reverse(expected[1])
+            || expected[0] === expected[1]
+            || (expected[0] === '01' && expected[1] === '23')
+            || (expected[0] === '12' && expected[1] === '34')
+            || (expected[0] === '23' && expected[1] === '45')
+            || (expected[0] === '13' && expected[1] === '37');
     }
 
     /**
@@ -39,7 +64,9 @@ module.exports = function (config, page, helpers) {
     return {
         push: function process(messages) {
             for (var i = 0; i < messages.length; i += 1) {
-                if (isHourMessage(messages[i].message) && isMessageMatchingRealHour(messages[i].message)) {
+                if (isHourMessage(messages[i].message)
+                    && isMessageMatchingRealHour(messages[i].message)
+                    && isHourSpecial(messages[i].message)) {
                     grat(messages[i].user);
                 }
             }
