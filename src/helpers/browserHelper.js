bcbb5e4ea3142e9681a4ac2ec3e8ff96ae40253b
Anis Safine Laget <anis@users.noreply.github.com>
2019-05-01 20:35:28 +0200

    Fix line jumps when typing in div

--- src/helpers/browserHelper.js
+++ src/helpers/browserHelper.js
@@ -301,7 +301,7 @@ module.exports = function (config, page) {
                         el.value = str;
                         el.focus();
                     } else {
-                        el.innerHTML = str;
+                        el.innerHTML = str.replace("\n", '<br/>');
                     }
 
                     if (submit === true) {
