c9c77938cf247df1ccc8dd46a239aa69800772eb
Anis Safine Laget <anis@users.noreply.github.com>
2019-07-18 20:55:10 +0200

    Improve grat.stats to handle deuce and stop mentioning

--- src/lib/requestHandlers/grat.stats.js
+++ src/lib/requestHandlers/grat.stats.js
@@ -3,11 +3,19 @@ var fs = require('fs');
 module.exports = function (config, page, helpers) {
     function insert(list, userStats) {
         var i = 0;
-        while (i < list.length && list[i].total >= userStats.total) {
+        while (i < list.length && list[i].total > userStats.total) {
             i += 1;
         }
 
-        return list.slice(0,i).concat([ userStats ]).concat(list.slice(i));
+        if (i >= list.length) {
+            return list.push([ userStats ]);
+        }
+
+        if (list[i][0].total === userStats.total) {
+            returnlist[i].push(userStats);
+        }
+
+        return list.slice(0,i).concat([[userStats]]).concat(list.slice(i));
     }
 
     function build(username, stats) {
@@ -48,14 +56,14 @@ module.exports = function (config, page, helpers) {
             }
 
             if (ordered.length === 0) {
-                helpers.chatango.message.send('Il n\'y a pas encore de classement pour le moment...');
+                helpers.chatango.message.send('Il n\'y a pas de classement pour le moment...');
                 return;
             }
 
             var leaderboard = " \n \n _____\n";
             var lines = [];
             for (var i = 0; i < ordered.length; i += 1) {
-                lines.push((i + 1) + '. @' + ordered[i].user + ' avec ' + ordered[i].total + ' gratouille' + (ordered[i].total > 1 ? 's' : ''));
+                lines.push((i + 1) + '. ' + ordered[i].map(({ user }) => user).join(', ') + ' avec ' + ordered[i][0].total + ' gratouille' + (ordered[i][0].total > 1 ? 's' : ''));
             }
 
             helpers.chatango.message.send(leaderboard + lines.join("\n"));
